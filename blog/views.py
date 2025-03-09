873315c2ee772e19e0a294853af7da67330b1f00
vladimir <virt.p.q@yandex.ru>
2025-03-09 20:25:32 +0300

    added comments for posts

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.conf import settings
 from blog.models import Post
-from blog.forms import EmailPostForm
+from blog.forms import EmailPostForm, CommentForm
 
 # Create your views here.
 
@@ -29,7 +30,9 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
         )
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
 
 
 def post_share(request, post_id):
@@ -52,6 +55,17 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+def post_comment(request, post_id):
+    post = get_object_or_404(Post.published, pk=post_id)
+    comment = None
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
 
 
 class PostListView(ListView):
